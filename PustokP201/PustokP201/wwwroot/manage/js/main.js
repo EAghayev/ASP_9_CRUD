010d740e82e34e07cfe3e4ff9847689fbb47794d
EAghayev <eaghayevv@gmail.com>
2021-12-24 18:12:32 +0400

    project added

---
+++ PustokP201/PustokP201/wwwroot/manage/js/main.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+
+    $(document).on("click", ".delete-btn", function (e) {
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log("deleted")
+
+            }
+            else {
+                console.log("cancel")
+            }
+        })
+
+    })
+
+
+})
+
